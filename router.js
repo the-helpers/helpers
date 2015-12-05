ba34a6803252f018c33ab46a59333a12b38e7102
Konrad Reiche <konrad@mixlr.com>
2015-12-05 15:24:52 +0000

    Add body-parser middleware to parse question

--- router.js
+++ router.js
@@ -1,9 +1,18 @@
 "use strict";
 
-const express  = require('express');
-const router   = express.Router();
+const express    = require('express');
+const bodyParser = require('body-parser');
+const router     = express.Router();
 
+router.use(bodyParser.urlencoded({ extended: false }));
 router.use('/', express.static('public'));
 router.use('/js', express.static('dist'));
+
+router.post('/ask', function (req, res) {
+  if (req.body.question) {
+    console.log('Received a new question: ' + req.body.question);
+    // handle res.body.question
+  }
+});
 
 module.exports = router;
