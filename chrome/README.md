f1dc9855826088062b2e3eaa7423cde0ed3d5673
Julian Fleischer <julian.fleischer@gamegenetics.de>
2015-12-05 23:17:25 +0000

    inject script

---
+++ chrome/README.md
@@ -0,0 +1,1 @@
+The Chrome Extension for Helpers.
---
+++ chrome/inject.js
@@ -0,0 +1,4 @@
+var x = document.createElement("div");
+var t = document.createTextNode("HELLO INJECTIONS");
+x.appendChild(t);
+document.getElementById('helpers').appendChild(x);
