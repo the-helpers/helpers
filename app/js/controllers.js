029a752f0a3147091f02d5798fd2cc4c3c857ea2
Konrad Reiche <konrad@mixlr.com>
2015-12-05 20:48:59 +0000

    Start to wire in answer/ask flow

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -18,12 +18,19 @@ helpersApp.controller('AskCtrl', function ($scope, $http) {
       $scope.Question = "";
     }
   };
+
+  var socket = io.connect();
+  socket.on('answer', function () {
+    alert("It's a match!");
+  });
 });
 
 helpersApp.controller('QuestionsCtrl', function($scope) {
-  var socket = io.connect();
   $scope.questions = [];
+  $scope.answerQuestion = function (id) {
+  }
 
+  var socket = io.connect();
   socket.on('connect', function () {
     console.log('Connected to backend');
   });
--- modules/routes/ask.js
+++ modules/routes/ask.js
@@ -4,7 +4,7 @@ module.exports = function (router, oracle) {
 
   router.post('/ask', function (req, res) {
     if (req.body.question) {
-      oracle.answer(req.body.question);
+      oracle.ask(req.body.question);
       res.sendStatus(200);
     } else {
       res.sendStatus(500);
