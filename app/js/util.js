c8da30b093bcfa8520f63a71eae987e0182bfafc
Konrad Reiche <konrad@mixlr.com>
2015-12-06 01:41:57 +0000

    Add util.js

---
+++ app/js/util.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var util = angular.module('util', []);
+
+util.askedAgo = function(ttl) {
+  var since = new Date().getSeconds() - (300 - ttl);
+  var asked = new Date();
+  asked.setSeconds(since);
+
+  var seconds = Math.floor((new Date() - asked) / 1000);
+
+  var interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) {
+    return interval + " years";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
+};
+
