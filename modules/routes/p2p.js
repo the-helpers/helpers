fcbb519db4edb2ff4deb57de312de8dc4c2be5e5
Julian Fleischer <julian.fleischer@gamegenetics.de>
2015-12-06 04:35:13 +0000

    good call bad call

--- modules/routes/p2p.js
+++ modules/routes/p2p.js
@@ -2,11 +2,12 @@
 
 module.exports = function (app, server, peer, config, chalk, logger) {
 	
-	var ExpressPeerServer = peer.ExpressPeerServer;
+	var PeerServer = peer.PeerServer;
 	var options = config.get('p2p');
 	
-	var path = '/p2p';
+	var peerServer = PeerServer({ port: 9000, path: '/' });
 	
-	app.use(path, ExpressPeerServer(server, options));
-	logger.info(chalk.green("p2p server set up at %s"), path);
+	logger.info(chalk.green("p2p server set up at :9000"));
+	
+	return peerServer;
 }
